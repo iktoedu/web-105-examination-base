5eb450db9d5187bb98a5efc40b8f86d983fc5b4c
Maxim Polishchuk <mpolishchuck@ikto.net>
2018-06-14 00:11:59 +0300

    Inital commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# How to start
+
+1. Make a copy of *.env.example* to *.env*
+2. Set database parameters in the *.env*
+3. Import database dump into your db
+4. Generate application key
+
+```bash
+$ php artisan key:generate
+```
+
+Proceed with your objectives!
---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('base')
+
+@section('content')
+    <table class="table">
+        <thead>
+        <tr>
+            <th>ID</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>&mdash;</td>
+        </tr>
+        </tbody>
+    </table>
+@endsection
