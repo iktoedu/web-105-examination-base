59313a6c2ef7afd61684e7c39cc84ff8016d49bb
rdinis322 <nazarvozniy322@gmail.com>
2018-06-14 14:29:22 +0300

    deleteButton

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,29 @@
 @section('content')
     <table class="table">
         <thead>
-        <tr>
-            <th>ID</th>
+            @foreach($animal as $animal)
+       
+		<tr>
+            <td> {{ $animal->title }}</td>
+            <td> {{ $animal->limbs_count }}</td>
+			<td><form action="{{ route('animalDelete',['animal' => $animal->id]) }}" method="post">
+        {{method_field('DELETE')}}
+        {{ csrf_field() }}
+       
+        <button type="submit" class="btn btn-danger">
+         Delete
+        </button></td>
         </tr>
+        @endforeach
         </thead>
         <tbody>
         <tr>
-            <td>&mdash;</td>
+		 <b><th>Назва тварини</th>
+		<th>Кількість кінцівок</th></b>
+            <td></td>
         </tr>
         </tbody>
     </table>
+	
+	 
 @endsection
