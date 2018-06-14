fc80c5a764a937aa92037a0b2f6495d8a21c4075
Alex_Jam <skerip@ex.ua>
2018-06-14 11:41:33 +0300

    first commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,14 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use App\Book;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index () {
+      $books = Book::select(['author','title','pages'])->get();
+      return view('index')->with(['books' => $books]);
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,21 @@
 @extends('base')
 
 @section('content')
-    <table class="table">
-        <thead>
-        <tr>
-            <th>ID</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>&mdash;</td>
-        </tr>
-        </tbody>
-    </table>
+        <table class="table">
+            <tr>
+                <th>Автор</th>
+                <th>Назва</th>
+                <th>Жанр</th>
+            </tr>
+            <thead>
+            @foreach($books as $books)
+            <tr>
+                <td>{{ $books -> author }}</td>
+                <td>{{ $books -> title }}</td>
+                <td>{{ $books -> pages }}</td>
+            </tr>
+            @endforeach
+            </thead>
+        </table>
+
 @endsection
